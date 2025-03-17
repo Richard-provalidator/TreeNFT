533d776470bf1c6552fe8ebdd0bd85e184b5cf03
richard <richard@provalidator.com>
2025-03-17 10:15:31 +0900

    Check Claim Time

--- dapp/src/components/Header.tsx
+++ dapp/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { JsonRpcSigner } from "ethers";
-import Logo from "./Logo.tsx";
+import Logo from "./Logo";
 
 interface HeaderProps {
   signer: JsonRpcSigner | null;
@@ -10,24 +10,17 @@ interface HeaderProps {
 
 function Header({ signer, connectWallet, disconnectWallet }: HeaderProps) {
   return (
-    <Box as="header" bgColor="blue.100" p={4}>
+    <Box as="header" p={4}>
       <Flex
         maxW={1024}
         mx="auto"
-        bgColor="red.100"
         alignItems="center"
         justifyContent="space-between"
       >
         <Logo />
         <Box>
           {signer ? (
-            <Text
-              bgColor="green.300"
-              px={2}
-              py={1}
-              cursor="pointer"
-              onClick={disconnectWallet}
-            >
+            <Text px={2} py={1} cursor="pointer" onClick={disconnectWallet}>
               {signer.address.substring(0, 7)}...
               {signer.address.substring(signer.address.length - 5)}
             </Text>
